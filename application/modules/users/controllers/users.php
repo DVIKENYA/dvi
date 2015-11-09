352a80b3e61ceff9856e6c999ccdfea221c04d7b
otienojulie <otienojulie@gmail.com>
2015-11-09 20:13:49 +0300

    Configures the config file to small caps checks in the users index function, the loggedin session status before loading the login page otherwise loads the dashboard

--- application/modules/users/controllers/users.php
+++ application/modules/users/controllers/users.php
@@ -10,8 +10,12 @@ class Users extends MY_Controller
     function index(){
         $data['module']="users";
         $data['view_file']="login_form";
-        $data['main_title'] = $this->get_title();
-        echo Modules::run('template/home', $data); 
+        $data['main_title'] = $this->get_title();   
+        if(!isset($this->session->userdata['logged_in'])){
+          echo Modules::run('template/home', $data); 
+        }else{
+          redirect('dashboard/home'); 
+        }
     }
 
     function list_users(){
